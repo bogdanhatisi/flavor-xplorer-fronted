0902828ada1fcb2e0417f94ed4baf7a688a083ee
Hatisi Bogdan <62288167+bogdanhatisi@users.noreply.github.com>
2024-01-09 01:31:33 +0200

    Initialization
    works

--- flavor-xplorer/src/app/app.module.ts
+++ flavor-xplorer/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { UserServiceComponent } from './services/user-service/user-service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
+    UserServiceComponent
   ],
   imports: [
     BrowserModule,
